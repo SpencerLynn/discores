94619a977c3c5574266d76c69de42f6280345750
Spencer Lynn <sllynn8907@gmail.com>
2014-08-28 14:41:28 -0400

    Setting up route to save course

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 var express = require('express'),
     courses = require('./routes/courses.js'),
-    scores = require('./routes/scores.js');
+    scores = require('./routes/scores.js'),
+    bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParse.json());
 app.use('/courses', courses);
 app.use('/scores', scores);
 
--- app/routes/courses.js
+++ app/routes/courses.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var courseManager = require('../../lib/courseManager.js');
 
 router.get('/', function(req, res) {
   res.writeHead(200);
   res.end("It's alive!");
+});
+
+router.post('/', function(req, res) {
+  var body = req.body;
+
+  courseManager.save(body).then(function(blob) {
+    res.writeHead(200, { 'content-type': 'application/json' });
+    res.write(JSON.stringify(blob));
+    res.end();
+  });
 });
 
 module.exports = router;

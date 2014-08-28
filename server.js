4f43494274d3659b56a7538309089f051dad619b
Spencer Lynn <sllynn8907@gmail.com>
2014-08-28 08:51:45 -0400

    Changing default port to 3000 instead of 80. Also renamed app/server.js to app/app.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-var app = require('./app/server.js');
+var app = require('./app/app.js');
 
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);

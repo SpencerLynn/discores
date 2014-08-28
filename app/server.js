108b6b60e49448f81c12682a657d887768b42c71
Spencer Lynn <sllynn8907@gmail.com>
2014-08-27 22:47:55 -0400

    Adding a '/scores' route to get scores.

--- app/server.js
+++ app/server.js
@@ -1,7 +1,9 @@
 var express = require('express'),
-    courses = require('./routes/courses.js');
+    courses = require('./routes/courses.js'),
+    scores = require('./routes/scores.js');
 
 var app = express();
 app.use('/courses', courses);
+app.use('/scores', scores);
 
 module.exports = app;

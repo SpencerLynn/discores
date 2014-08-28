3f3f58d1d820aebdfe16ffe73866310b3e0b78ad
Spencer Lynn <sllynn8907@gmail.com>
2014-08-28 16:19:02 -0400

    Implemented getting course list

--- spec/courseManager.js
+++ spec/courseManager.js
@@ -7,22 +7,42 @@ describe('courseManager', function() {
     expect(courseManager).toBeDefined();
   });
 
-  describe('createCourse', function() {
+  describe('saving a course', function() {
     beforeEach(function() {
-      try{
       spyOn(blobService, 'createBlockBlobFromText')
-        .and.callFake(function(c, n, o, cb) {
-          cb(null, { done: true }, null);
-        }); } catch (e) { console.log(e); }
+        .and.callFake(function(container, name, course, cb) {
+          cb(null, { done: true });
+        });
     });
 
-    it('should create a course', function(done) {
+    it('should save a course', function(done) {
       var testCourse = { name: 'TestCourseName' };
       courseManager.save(testCourse).done(function() {
         var calls = blobService.createBlockBlobFromText.calls;
         expect(calls.count()).toEqual(1);
         expect(calls.argsFor(0)[0]).toEqual('courses');
         expect(calls.argsFor(0)[2]).toEqual(JSON.stringify(testCourse));
+        done();
+      });
+    });
+  });
+
+  describe('getting courses', function() {
+    beforeEach(function() {
+      spyOn(blobService, 'getBlobToText')
+        .and.callFake(function(container, blob, cb) {
+          cb(null, '[{ "name": "test" }]');
+        });
+    });
+
+    it('should get course list', function(done) {
+      courseManager.query().then(function(courses) {
+        var calls = blobService.getBlobToText.calls;
+        expect(calls.count()).toEqual(1);
+        expect(calls.argsFor(0)[0]).toEqual('courses');
+        expect(calls.argsFor(0)[1]).toEqual('index');
+        expect(courses.length).toEqual(1);
+        expect(courses[0].name).toEqual('test');
         done();
       });
     });

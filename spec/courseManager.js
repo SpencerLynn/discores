a46770c6765687f81bd32e6974cb90e62105cc90
Spencer Lynn <sllynn8907@gmail.com>
2014-08-28 14:33:17 -0400

    Course manager save and tested

--- spec/courseManager.js
+++ spec/courseManager.js
@@ -1,14 +1,30 @@
-var courseManager = require('../lib/courseManager.js');
+var azure = require('azure-storage');
+var blobService = azure.createBlobService('test', new Buffer('test').toString('base64'));
+var courseManager = require('../lib/courseManager.js')(blobService);
 
 describe('courseManager', function() {
   it('exists', function() {
     expect(courseManager).toBeDefined();
   });
 
-  // describe('createCourse', function(){
-  //   it('should ensure that container exists', function(){
-  //     expect(yaks.shave).toBeDefined();
-  //   });
-  // });
+  describe('createCourse', function() {
+    beforeEach(function() {
+      try{
+      spyOn(blobService, 'createBlockBlobFromText')
+        .and.callFake(function(c, n, o, cb) {
+          cb(null, { done: true }, null);
+        }); } catch (e) { console.log(e); }
+    });
 
+    it('should create a course', function(done) {
+      var testCourse = { name: 'TestCourseName' };
+      courseManager.save(testCourse).done(function() {
+        var calls = blobService.createBlockBlobFromText.calls;
+        expect(calls.count()).toEqual(1);
+        expect(calls.argsFor(0)[0]).toEqual('courses');
+        expect(calls.argsFor(0)[2]).toEqual(JSON.stringify(testCourse));
+        done();
+      });
+    });
+  });
 });

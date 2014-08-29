1d2d3a4ade0943b6fbc16a947aa8e31dbac98ef5
Spencer Lynn <sllynn8907@gmail.com>
2014-08-29 13:41:07 -0400

    UI for seeing courses.

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 var app = express();
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use('/courses', courses);
 app.use('/scores', scores);
---
+++ public/scripts/discores.js
@@ -0,0 +1,25 @@
+(function() {
+  var app = angular.module('discores', ['ngRoute']);
+
+  // app.config(['$routeProvider', function($routeProvider) {
+  //   $routeProvider.otherwise('/courses/:course', {
+  //     templateUrl: '/views/courses.html',
+  //     controller: 'CourseCtrl',
+  //     controllerAs: 'courseCtrl'
+  //   });
+  // }]);
+
+  app.controller('CourseCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    var self = this;
+
+    self.courses = [];
+
+    $http.get('/courses')
+      .success(function(data) {
+        self.courses.length = 0;
+        data.forEach(function(c) {
+          self.courses.push(c);
+        });
+      });
+  }]);
+})();

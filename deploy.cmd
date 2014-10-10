6401c6d674ceec4c44ebf5289a8ba6bb7cf3531f
Spencer Lynn <sllynn8907@gmail.com>
2014-10-10 17:01:14 -0500

    Debugging deployment

--- deploy.cmd
+++ deploy.cmd
@@ -108,9 +108,8 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Install bower packages
 IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  ./node_modules/.bin/bower install
-  IF !ERRORLEVEL! NEQ 0 goto error
-
+  ::./node_modules/.bin/bower install
+  ::IF !ERRORLEVEL! NEQ 0 goto error
   popd
 fi
 
